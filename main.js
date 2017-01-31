5711c93ad51851ff680c10c6bcab8e9a5e317563
Richard Mands <richard@eduku.org>
2017-01-31 14:20:47 +0800

    js update

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 console.log('Hello!');
-let x = document.getElementById("q1")
-let text = ""
-for (var i = 0; i < x.length; i++) {
-  text += x.elements[i].value + "<br>"
-}
-console.log('text: ', text)
+let x = document.getElementById('q1')
+let length = x.length
+console.log('length: ', length)
+console.log('x: ', x)
+
+let num1 = document.getElementById('q1num1').value
+let sign = document.getElementById('q1sign').value
+let num2 = document.getElementById('q1num2').value
+
+console.log('num1, sign, num2: ', num1, sign, num2)
