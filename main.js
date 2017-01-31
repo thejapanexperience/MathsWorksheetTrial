400fa34954c13d3f9d0fbf0535c1237f233deb23
Richard Mands <richard@eduku.org>
2017-01-31 14:28:48 +0800

    update js

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ let length = x.length
 console.log('length: ', length)
 console.log('x: ', x)
 
-let num1 = document.getElementById('q1num1').value
-let sign = document.getElementById('q1sign').value
-let num2 = document.getElementById('q1num2').value
+let num1 = document.getElementById('q1num1').innerHTML
+let sign = document.getElementById('q1sign').innerHTML
+let num2 = document.getElementById('q1num2').innerHTML
 
 console.log('num1, sign, num2: ', num1, sign, num2)
+
+console.log('num1.value: ', num1.value)
+console.log('num1: ', num1)
