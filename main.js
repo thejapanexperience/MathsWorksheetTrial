064c9500e87c7daf9ed29dfa324191132ef27c90
Richard Mands <richard@eduku.org>
2017-01-31 14:48:13 +0800

    update js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 console.log('Hello!');
-let x = document.getElementById('q1')
-let length = x.length
-console.log('length: ', length)
-console.log('x: ', x)
 
-let num1 = parseInt(document.getElementById('q1num1').innerHTML)
-let sign = document.getElementById('q1sign').innerHTML
-let num2 = parseInt(document.getElementById('q1num2').innerHTML)
-let ans
-console.log('num1, sign, num2: ', num1, sign, num2)
-if (sign === '+') {
-   ans = num1 + num2
-} else if (sign === '-') {
-   ans = num1 - num2
+let numberOfQuestions = 1
+
+for (var i = 1; i < numberOfQuestions + 1; i++) {
+  console.log('i: ', i)
+  let num1 = parseInt(document.getElementById(`q${i}num1`).innerHTML)
+  let sign = document.getElementById(`q${i}sign`).innerHTML
+  let num2 = parseInt(document.getElementById(`q${i}num2`).innerHTML)
+  let ans = []
+  console.log('num1, sign, num2: ', num1, sign, num2)
+  if (sign === '+') {
+    ans[i] = num1 + num2
+  } else if (sign === '-') {
+    ans[i] = num1 - num2
+  }
+  console.log('ans: ', ans)
 }
-console.log('ans: ', ans)
