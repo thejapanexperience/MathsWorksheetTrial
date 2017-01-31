1fd089860a93bf34b5b842e0d05bf8176ebee6de
Richard Mands <richard@eduku.org>
2017-01-31 14:38:42 +0800

    update js

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ let length = x.length
 console.log('length: ', length)
 console.log('x: ', x)
 
-let num1 = document.getElementById('q1num1').innerHTML
+let num1 = parseInt(document.getElementById('q1num1').innerHTML)
 let sign = document.getElementById('q1sign').innerHTML
-let num2 = document.getElementById('q1num2').innerHTML
-
+let num2 = parseInt(document.getElementById('q1num2').innerHTML)
+let ans
 console.log('num1, sign, num2: ', num1, sign, num2)
-
-console.log('num1.value: ', num1.value)
-console.log('num1: ', num1)
+if (sign === '+') {
+   ans = num1 + num2
+} else if (sign === '-') {
+   ans = num1 - num2
+}
+console.log('ans: ', ans)
