3d2b49bc218aa88b4af707491e6bc121ac7a5d49
Richard Mands <richard@eduku.org>
2017-02-01 10:47:09 +0800

    prevent form resubmission

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ console.log('Hello!');
 
 let numberOfQuestions = 2
 
+let ans = {}
+
 for (var i = 1; i < numberOfQuestions + 1; i++) {
   console.log('i: ', i)
   let num1 = parseInt(document.getElementById(`q${i}num1`).innerHTML)
   let sign = document.getElementById(`q${i}sign`).innerHTML
   let num2 = parseInt(document.getElementById(`q${i}num2`).innerHTML)
-  let ans = {}
   console.log('num1, sign, num2: ', num1, sign, num2)
   if (sign === '+') {
     ans[i] = num1 + num2
