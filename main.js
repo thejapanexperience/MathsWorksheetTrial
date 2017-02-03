fe5869d94adc4ea5b6c647f594bdd5b1f3967d9d
Richard Mands <richard@eduku.org>
2017-02-03 15:06:39 +0800

    working

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-console.log('Hello!');
-
-let numberOfQuestions = 2
-
 let ans = {}
 let score = 0
 let completed = 0
 
-for (var i = 1; i < numberOfQuestions + 1; i++) {
-  console.log('i: ', i)
+for (var i = 1; i < 99; i++) {
+  if (document.getElementById(`q${i}num1`) === null){
+    break
+  }
   let num1 = parseInt(document.getElementById(`q${i}num1`).innerHTML)
   let sign = document.getElementById(`q${i}sign`).innerHTML
   let num2 = parseInt(document.getElementById(`q${i}num2`).innerHTML)
-  console.log('num1, sign, num2: ', num1, sign, num2)
   if (sign === '+') {
     ans[i] = num1 + num2
   } else if (sign === '-') {
     ans[i] = num1 - num2
+  } else if (sign === 'x') {
+    ans[i] = num1 * num2
+  }else if (sign === '÷') {
+    ans[i] = num1 / num2
   }
   console.log('ans: ', ans)
 }
